fa7eb7bde4261bb5ce46996efd906c0a85c0f4a5
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2024-02-07 16:08:11 +0000

    feat(liquidationVisibility): vaultFactory manifest and proposal-builder

---
+++ packages/builders/scripts/inter-protocol/liquidationVisibility/manifest.js
@@ -0,0 +1,60 @@
+import { startVaultFactory } from '@agoric/inter-protocol/src/proposals/econ-behaviors.js';
+import { Stable } from '@agoric/internal/src/tokens.js';
+
+const VAULT_FACTORY_MANIFEST = harden({
+  [startVaultFactory.name]: {
+    consume: {
+      board: 'board',
+      chainStorage: true,
+      diagnostics: true,
+      feeMintAccess: 'zoe',
+      chainTimerService: 'timer',
+      zoe: 'zoe',
+      priceAuthority: 'priceAuthority',
+      economicCommitteeCreatorFacet: 'economicCommittee',
+      reserveKit: 'reserve',
+      auctioneerKit: 'auction',
+    },
+    produce: { vaultFactoryKit: 'VaultFactory' },
+    brand: { consume: { [Stable.symbol]: 'zoe' } },
+    oracleBrand: { consume: { USD: true } },
+    installation: {
+      consume: {
+        contractGovernor: 'zoe',
+        VaultFactory: 'zoe',
+      },
+    },
+    instance: {
+      consume: {
+        reserve: 'reserve',
+        auctioneer: 'auction',
+      },
+      produce: {
+        VaultFactory: 'VaultFactory',
+        VaultFactoryGovernor: 'VaultFactoryGovernor',
+      },
+    },
+  },
+});
+
+export const getManifestForVaultFactory = (
+  { restoreRef },
+  {
+    installKeys,
+    vaultFactoryControllerAddress,
+    minInitialPoolLiquidity,
+    referencedUi,
+  },
+) => {
+  return {
+    manifest: VAULT_FACTORY_MANIFEST,
+    installations: {
+      VaultFactory: restoreRef(installKeys.vaultFactory),
+    },
+    options: {
+      vaultFactoryControllerAddress,
+      minInitialPoolLiquidity,
+      referencedUi,
+    },
+  };
+};
---
+++ packages/builders/scripts/inter-protocol/liquidationVisibility/proposal-builder.js
@@ -0,0 +1,71 @@
+import { makeHelpers } from '@agoric/deploy-script-support';
+import { objectMap } from '@agoric/internal';
+import { makeInstallCache } from '@agoric/inter-protocol/src/proposals/utils.js';
+import { getManifestForVaultFactory } from './manifest.js';
+
+/** @type {Record<string, Record<string, [string, string]>>} */
+const installKeys = {
+  vaultFactory: [
+    '@agoric/inter-protocol/src/vaultFactory/vaultFactory.js',
+    '@agoric/builders/scripts/inter-protocol/liquidationVisibility/bundles/bundle-vaultFactory.js',
+  ],
+};
+
+/**
+ * @template I
+ * @template R
+ * @param {object} opts
+ * @param {(i: I) => R} opts.publishRef
+ * @param {(m: string, b: string, opts?: any) => I} opts.install
+ * @param {<T>(f: T) => T} [opts.wrapInstall]
+ */
+export const vaultFactoryUpdateProposalBuilder = async ({
+  publishRef,
+  install: install0,
+  wrapInstall,
+}) => {
+  const { VAULT_FACTORY_CONTROLLER_ADDR } = process.env;
+
+  const install = wrapInstall ? wrapInstall(install0) : install0;
+
+  const persist = true;
+  /** @param {Record<string, [string, string]>} group */
+  const publishGroup = group =>
+    objectMap(group, ([mod, bundle]) =>
+      publishRef(install(mod, bundle, { persist })),
+    );
+
+  return harden({
+    sourceSpec: './manifest.js',
+    getManifestCall: [
+      getManifestForVaultFactory.name,
+      {
+        vaultFactoryControllerAddress: VAULT_FACTORY_CONTROLLER_ADDR,
+        installKeys: {
+          ...publishGroup(installKeys),
+        },
+      },
+    ],
+  });
+};
+
+export default async (homeP, endowments) => {
+  const helperEndowments = {
+    ...endowments,
+    cacheDir: endowments.pathResolve(process.cwd(), 'cache'),
+  };
+  const { writeCoreProposal } = await makeHelpers(homeP, helperEndowments);
+
+  const tool = await makeInstallCache(homeP, {
+    loadBundle: spec => import(spec),
+  });
+
+  await writeCoreProposal('vaultFactory', opts =>
+    vaultFactoryUpdateProposalBuilder({
+      ...opts,
+      wrapInstall: tool.wrapInstall,
+    }),
+  );
+
+  await tool.saveCache();
+};
