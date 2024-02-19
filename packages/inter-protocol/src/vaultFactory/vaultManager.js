f43b2efeb25a87c1bd5f05a76e2cd7e4e5c4aea8
anilhelvaci <anilhelvaci96@gmail.com>
2024-02-19 22:45:47 +0300

    parent 1f641d9bcdf35161e0ca053742918c29a2f53090 author anilhelvaci <anilhelvaci96@gmail.com> 1708371947 +0300 committer anilhelvaci <anilhelvaci96@gmail.com> 1708960452 +0300
    fix(liquidationVisibility): lint fix
    
    fix(liquidationVisibility): lint fix
    
    fix(liquidationVisibility): lint fix
    
    fix(liquidationVisibility): fix paths
    
    fix(liquidationVisibility): lint and type fixes
    
    fix(liquidationVisibility): lint fixes

--- packages/inter-protocol/src/vaultFactory/vaultManager.js
+++ packages/inter-protocol/src/vaultFactory/vaultManager.js
@@ -181,7 +181,7 @@ const trace = makeTracer('VM');
  *   mintedProceeds?: Amount<'nat'>;
  *   collateralSold?: Amount<'nat'>;
  *   collateralRemaining?: Amount<'nat'>;
- *   endTime?: import('@agoric/time').TimestampRecord | null;
+ *   endTime?: import('@agoric/time/src/types.js').TimestampRecord | null;
  * }} AuctionResultState
  *
  * @typedef {{
@@ -694,7 +694,6 @@ export const prepareVaultManagerKit = (
               collateralSold: plan.collateralSold,
               collateralRemaining: plan.collatRemaining,
               // @ts-expect-error
-              // eslint-disable-next-line @endo/no-optional-chaining
               endTime: auctionSchedule?.liveAuctionSchedule.endTime,
             };
             return E(
