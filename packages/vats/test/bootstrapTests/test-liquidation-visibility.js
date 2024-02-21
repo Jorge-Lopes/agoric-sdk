67082c12024075548a0a3a21d3f820130c94c7a3
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2024-02-21 19:18:32 +0000

    fix(liquidationVisibility): remove outdated tests and methods

--- packages/vats/test/bootstrapTests/test-liquidation-visibility.js
+++ packages/vats/test/bootstrapTests/test-liquidation-visibility.js
@@ -4,14 +4,10 @@
  * @file Bootstrap test vaults liquidation visibility
  */
 import { test as anyTest } from '@agoric/zoe/tools/prepare-test-env-ava.js';
-import { makeLiquidationTestContext } from './liquidation.js';
-import {
-  addNewVaults,
-  checkVisibility,
-  ensureVaultCollateral,
-  initVaults,
-  startAuction,
-} from './liquidation-visibility-utils.js';
+import { NonNullish } from '@agoric/assert/src/assert.js';
+import { Offers } from '@agoric/inter-protocol/src/clientSupport.js';
+import { TimeMath } from '@agoric/time';
+import { makeLiquidationTestContext, scale6 } from './liquidation.js';
 
 /**
  * @type {import('ava').TestFn<Awaited<ReturnType<typeof makeLiquidationTestContext>>>}
@@ -91,22 +87,233 @@ const outcome = /** @type {const} */ ({
 });
 //#endregion
 
+const placeBids = async (
+  t,
+  collateralBrandKey,
+  buyerWalletAddress,
+  base = 0, // number of bids made before
+) => {
+  const { agoricNamesRemotes, walletFactoryDriver, readLatest } = t.context;
+
+  const buyer = await walletFactoryDriver.provideSmartWallet(
+    buyerWalletAddress,
+  );
+
+  await buyer.sendOffer(
+    Offers.psm.swap(
+      agoricNamesRemotes,
+      agoricNamesRemotes.instance['psm-IST-USDC_axl'],
+      {
+        offerId: `print-${collateralBrandKey}-ist`,
+        wantMinted: 1_000,
+        pair: ['IST', 'USDC_axl'],
+      },
+    ),
+  );
+
+  const maxBuy = `10000${collateralBrandKey}`;
+
+  for (let i = 0; i < setup.bids.length; i += 1) {
+    const offerId = `${collateralBrandKey}-bid${i + 1 + base}`;
+    // bids are long-lasting offers so we can't wait here for completion
+    await buyer.sendOfferMaker(Offers.auction.Bid, {
+      offerId,
+      ...setup.bids[i],
+      maxBuy,
+    });
+    t.like(readLatest(`published.wallet.${buyerWalletAddress}`), {
+      status: {
+        id: offerId,
+        result: 'Your bid has been accepted',
+        payouts: undefined,
+      },
+    });
+  }
+};
+
+const runAuction = async (runUtils, advanceTimeBy) => {
+  const { EV } = runUtils;
+  const auctioneerKit = await EV.vat('bootstrap').consumeItem('auctioneerKit');
+  const { liveAuctionSchedule } = await EV(
+    auctioneerKit.publicFacet,
+  ).getSchedules();
+
+  await advanceTimeBy(3 * Number(liveAuctionSchedule.steps), 'minutes');
+
+  return liveAuctionSchedule;
+};
+
+const setupVaults = async (t, collateralBrandKey, managerIndex, base = 0) => {
+  const { setupStartingState, walletFactoryDriver, check } = t.context;
+
+  await setupStartingState();
+
+  const minter = await walletFactoryDriver.provideSmartWallet('agoric1minter');
+
+  for (let i = 0; i < setup.vaults.length; i += 1) {
+    const offerId = `open-${collateralBrandKey}-vault${base + i}`;
+    await minter.executeOfferMaker(Offers.vaults.OpenVault, {
+      offerId,
+      collateralBrandKey,
+      wantMinted: setup.vaults[i].ist,
+      giveCollateral: setup.vaults[i].atom,
+    });
+    t.like(minter.getLatestUpdateRecord(), {
+      updated: 'offerStatus',
+      status: { id: offerId, numWantsSatisfied: 1 },
+    });
+  }
+
+  // Verify starting balances
+  for (let i = 0; i < setup.vaults.length; i += 1) {
+    check.vaultNotification(managerIndex, i, {
+      debtSnapshot: {
+        debt: { value: scale6(setup.vaults[i].debt) },
+      },
+      locked: { value: scale6(setup.vaults[i].atom) },
+      vaultState: 'active',
+    });
+  }
+};
+
+const startAuction = async t => {
+  const { readLatest, advanceTimeTo } = t.context;
+
+  const scheduleNotification = readLatest('published.auction.schedule');
+
+  await advanceTimeTo(NonNullish(scheduleNotification.nextStartTime));
+};
+
+const addNewVaults = async ({ t, collateralBrandKey, base }) => {
+  const { walletFactoryDriver, priceFeedDriver } = t.context;
+
+  await priceFeedDriver.setPrice(setup.price.starting);
+  const minter = await walletFactoryDriver.provideSmartWallet('agoric1minter');
+
+  for (let i = 0; i < setup.vaults.length; i += 1) {
+    const offerId = `open-${collateralBrandKey}-vault${base + i}`;
+    await minter.executeOfferMaker(Offers.vaults.OpenVault, {
+      offerId,
+      collateralBrandKey,
+      wantMinted: setup.vaults[i].ist,
+      giveCollateral: setup.vaults[i].atom,
+    });
+    t.like(minter.getLatestUpdateRecord(), {
+      updated: 'offerStatus',
+      status: { id: offerId, numWantsSatisfied: 1 },
+    });
+  }
+
+  await placeBids(t, collateralBrandKey, 'agoric1buyer', base);
+  await priceFeedDriver.setPrice(setup.price.trigger);
+  await startAuction(t);
+};
+
+const initVaults = async ({ t, collateralBrandKey, managerIndex }) => {
+  const { priceFeedDriver, readLatest } = t.context;
+
+  const metricsPath = `published.vaultFactory.managers.manager${managerIndex}.metrics`;
+
+  await setupVaults(t, collateralBrandKey, managerIndex);
+  await placeBids(t, collateralBrandKey, 'agoric1buyer');
+
+  await priceFeedDriver.setPrice(setup.price.trigger);
+  await startAuction(t);
+
+  t.like(readLatest(metricsPath), {
+    numActiveVaults: 0,
+    numLiquidatingVaults: setup.vaults.length,
+    liquidatingCollateral: {
+      value: scale6(setup.auction.start.collateral),
+    },
+    liquidatingDebt: { value: scale6(setup.auction.start.debt) },
+    lockedQuote: null,
+  });
+};
+
+const checkVisibility = async ({
+  t,
+  managerIndex,
+  setupCallback,
+  base = 0,
+}) => {
+  const { readLatest, advanceTimeBy, runUtils } = t.context;
+
+  await setupCallback();
+
+  const { startTime, startDelay, endTime } = await runAuction(
+    runUtils,
+    advanceTimeBy,
+  );
+
+  const nominalStart = TimeMath.subtractAbsRel(
+    startTime.absValue,
+    startDelay.relValue,
+  );
+  t.log(nominalStart);
+
+  const visibilityPath = `published.vaultFactory.managers.manager${managerIndex}.liquidations.${nominalStart.toString()}`;
+  const preAuction = readLatest(`${visibilityPath}.vaults.preAuction`);
+  const postAuction = readLatest(`${visibilityPath}.vaults.postAuction`);
+  const auctionResult = readLatest(`${visibilityPath}.auctionResult`);
+
+  const expectedPreAuction = [];
+  for (let i = 0; i < setup.vaults.length; i += 1) {
+    expectedPreAuction.push([
+      `vault${base + i}`,
+      {
+        collateralAmount: { value: scale6(setup.vaults[i].atom) },
+        debtAmount: { value: scale6(setup.vaults[i].debt) },
+      },
+    ]);
+  }
+  t.like(
+    Object.fromEntries(preAuction),
+    Object.fromEntries(expectedPreAuction),
+  );
+
+  const expectedPostAuction = [];
+  // Iterate from the end because we expect the post auction vaults
+  // in best to worst order.
+  for (let i = outcome.vaults.length - 1; i >= 0; i -= 1) {
+    expectedPostAuction.push([
+      `vault${base + i}`,
+      { Collateral: { value: scale6(outcome.vaults[i].locked) } },
+    ]);
+  }
+  t.like(
+    Object.fromEntries(postAuction),
+    Object.fromEntries(expectedPostAuction),
+  );
+
+  t.like(auctionResult, {
+    collateralOffered: { value: scale6(setup.auction.start.collateral) },
+    istTarget: { value: scale6(setup.auction.start.debt) },
+    collateralForReserve: { value: scale6(outcome.reserve.allocations.ATOM) },
+    shortfallToReserve: { value: 0n },
+    mintedProceeds: { value: scale6(setup.auction.start.debt) },
+    collateralSold: {
+      value:
+        scale6(setup.auction.start.collateral) -
+        scale6(setup.auction.end.collateral),
+    },
+    collateralRemaining: { value: 0n },
+    endTime: { absValue: endTime.absValue },
+  });
+
+  t.log('preAuction', preAuction);
+  t.log('postAuction', postAuction);
+  t.log('auctionResult', auctionResult);
+};
+
 test.before(async t => {
   t.context = await makeLiquidationTestContext(t);
 });
 
-/**
- * @file In this file we test the below scenario:
- * - Alice opens a vault
- * - Alice gets liquidated
- * - Visibility data correctly observed in storage
- * - Vault factory gets restarted
- * - An auction starts with no vaults to liquidate
- * - No unnecessary storage node is created when `liquidateVaults` is invoked with no vaults to liquidate
- * - Bob opens a vault
- * - Bob gets liquidated
- * - Visibility data correctly observed in storage
- */
+test.after.always(t => {
+  return t.context.shutdown && t.context.shutdown();
+});
+
 test.serial('visibility-before-upgrade', async t => {
   await checkVisibility({
     t,
@@ -116,17 +323,8 @@ test.serial('visibility-before-upgrade', async t => {
         t,
         collateralBrandKey: 'ATOM',
         managerIndex: 0,
-        setup,
       }),
-    setup,
-    outcome,
   });
-});
-
-test.serial('add-STARS-collateral', async t => {
-  await ensureVaultCollateral('STARS', t);
-  await t.context.setupStartingState();
-  t.pass(); // reached here without throws
 });
 
 test.serial('restart-vault-factory', async t => {
@@ -208,29 +406,12 @@ test.serial('visibility-after-upgrade', async t => {
       addNewVaults({
         t,
         collateralBrandKey: 'ATOM',
-        setup,
         base: setup.vaults.length,
       }),
-    setup,
-    outcome,
     base: 3,
   });
-});
 
-test.serial('here-check-STARS-visibility', async t => {
-  await checkVisibility({
-    t,
-    managerIndex: 1,
-    setupCallback: () =>
-      addNewVaults({
-        t,
-        collateralBrandKey: 'STARS',
-        setup,
-        base: 0,
-      }),
-    setup,
-    outcome,
-  });
+  t.pass();
 });
 
 test.serial('snapshot-storage', async t => {
@@ -250,17 +431,5 @@ test.serial('snapshot-storage', async t => {
     ['vaults.preAuction', 'vaults.postAuction', 'auctionResult'],
     0,
     3600n,
-  );
-
-  buildSnapshotItem(
-    ['vaults.preAuction', 'vaults.postAuction', 'auctionResult'],
-    0,
-    10800n,
-  );
-
-  buildSnapshotItem(
-    ['vaults.preAuction', 'vaults.postAuction', 'auctionResult'],
-    1,
-    14400n,
   );
 });
