65a65c04411446ae31395afc5c96a5574cf08c23
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2024-06-06 16:32:14 +0100

    feat(x/nft): extend NftKit with getBalances and send methods
    Ref: #33

--- packages/orchestration/src/exos/nftKit.js
+++ packages/orchestration/src/exos/nftKit.js
@@ -2,13 +2,18 @@ import { M } from '@endo/patterns';
 import { E } from '@endo/far';
 import { TopicsRecordShape } from '@agoric/zoe/src/contractSupport/index.js';
 import { ChainAddressShape, NftShape } from '../typeGuards.js';
+import { toRequestQueryJson } from '@agoric/cosmic-proto';
+import { Fail } from '@agoric/assert';
+import { decodeBase64 } from '@endo/base64';
+import { Any } from '@agoric/cosmic-proto/google/protobuf/any.js';
 import {
   QueryNFTsRequest,
   QueryNFTsResponse,
 } from '@agoric/cosmic-proto/cosmos/nft/v1beta1/query.js';
-import { toRequestQueryJson } from '@agoric/cosmic-proto';
-import { Fail } from '@agoric/assert';
-import { decodeBase64 } from '@endo/base64';
+import {
+  MsgSend,
+  MsgSendResponse,
+} from '@agoric/cosmic-proto/cosmos/nft/v1beta1/tx.js';
 
 /**
  * @param {import('@agoric/zone').Zone} zone
@@ -21,14 +26,12 @@ export const prepareNftKit = (zone, zcf) => {
       helper: M.interface('helper', {
         toNftDenomAmount: M.call(M.string()).returns(M.any()),
       }),
-      invitationMakers: M.interface('invitationMakers', {
-        send: M.call().returns(M.any()),
-      }),
       holder: M.interface('IcaAccountHolder', {
         getPublicTopics: M.call().returns(TopicsRecordShape),
         getAddress: M.call().returns(ChainAddressShape),
         getBalance: M.call(M.string()).returns(NftShape),
         getBalances: M.call().returns(M.arrayOf(NftShape)),
+        send: M.call().returns(M.any()),
       }),
     },
     /**
@@ -45,14 +48,9 @@ export const prepareNftKit = (zone, zcf) => {
       helper: {
         toNftDenomAmount(nft) {},
       },
-
-      invitationMakers: {
-        async send(nftAmount, address) {},
-      },
-
       holder: {
         getAddress() {
-          return this.state.chainAddress;
+          return this.state.chainAddress.address;
         },
         async getBalance(denom) {
           const { toNftDenomAmount } = this.facets.helper;
@@ -75,7 +73,43 @@ export const prepareNftKit = (zone, zcf) => {
 
           return harden(toNftDenomAmount(nfts));
         },
-        async getBalances() {},
+        async getBalances() {
+          const { toNftDenomAmount } = this.facets.helper;
+          const { chainAddress, icqConnection } = this.state;
+
+          /* Based on the protobuf definition for QueryNFTsRequest,
+           * it should be possible to provide only the owner as argument
+           */
+          const [result] = await E(icqConnection).query([
+            toRequestQueryJson(
+              QueryNFTsRequest.toProtoMsg({
+                owner: chainAddress.address,
+                classId: '',
+              }),
+            ),
+          ]);
+
+          if (!result?.key) throw Fail`Error parsing result ${result}`;
+          const { nfts } = QueryNFTsResponse.decode(decodeBase64(result.key));
+          if (!nfts) throw Fail`Result lacked balance key: ${result}`;
+
+          return harden(toNftDenomAmount(nfts));
+        },
+        async send(nftAmount, address) {
+          const { account } = this.state;
+          const classId = this.state.nftDenom || nftAmount.brand;
+
+          await E(account).executeEncodedTx([
+            Any.toJSON(
+              MsgSend.toProtoMsg({
+                classId: classId,
+                id: nftAmount.value.id,
+                sender: this.state.chainAddress.address,
+                receiver: address,
+              }),
+            ),
+          ]);
+        },
       },
     },
   );
