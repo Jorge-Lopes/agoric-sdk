01fd405027dba9d534a9a38a4515c4f3b01cd9ec
anilhelvaci <anilhelvaci96@gmail.com>
2024-06-17 18:28:54 +0300

    fixup(liquidationVisibility): Remove `allValuesSetteled()`
    Refs: #35

--- packages/internal/test/test-utils.js
+++ packages/internal/test/test-utils.js
@@ -13,7 +13,6 @@ import {
   forever,
   deeplyFulfilledObject,
   synchronizedTee,
-  allValuesSettled,
 } from '../src/utils.js';
 
 test('fromUniqueEntries', t => {
@@ -263,18 +262,4 @@ test('synchronizedTee - consume synchronized', async t => {
   t.is(i, 2);
   t.deepEqual(output1, sourceData.slice(0, i));
   t.deepEqual(output2, sourceData.slice(0, i));
-});
-
-test('allValuesSettled', async t => {
-  const result = await allValuesSettled({
-    promiseOne: Promise.resolve('I am a happy promise - One'),
-    promiseTwo: Promise.reject(new Error('I am an upset promise')),
-    promiseThree: Promise.resolve('I am a happy promise - Three'),
-  });
-
-  t.deepEqual(result, {
-    promiseOne: 'I am a happy promise - One',
-    promiseTwo: undefined,
-    promiseThree: 'I am a happy promise - Three',
-  });
 });
